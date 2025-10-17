56fdb26bd08caa878a4644da8d276ae44204e811
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-10-17 16:33:27 +0100

    add override for storage account name

--- components/tfplan-viewer/ai.tf
+++ components/tfplan-viewer/ai.tf
@@ -1,10 +1,11 @@
 module "ai" {
-  source                       = "git::https://github.com/hmcts/terraform-module-ai-services?ref=optional-resources"
-  env                          = var.env
-  project                      = "${var.component}-${var.env}-project"
-  existing_resource_group_name = azurerm_resource_group.rg.name
-  common_tags                  = module.ctags.common_tags
-  product                      = var.component
-  component                    = var.component
-  key_vault_id                 = module.keyvault.key_vault_id
+  source                        = "git::https://github.com/hmcts/terraform-module-ai-services?ref=optional-resources"
+  env                           = var.env
+  project                       = "${var.component}-${var.env}-project"
+  existing_resource_group_name  = azurerm_resource_group.rg.name
+  common_tags                   = module.ctags.common_tags
+  product                       = var.component
+  component                     = var.component
+  key_vault_id                  = module.keyvault.key_vault_id
+  storage_account_name_override = "${replace(var.component, "-", "")}foundrysa"
 }
