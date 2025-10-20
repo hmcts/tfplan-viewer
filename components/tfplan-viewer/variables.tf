36a5778c8b6bc84a4a50e053acb7f95a89e4ece6
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-10-20 09:52:19 +0100

    change var format

--- components/tfplan-viewer/variables.tf
+++ components/tfplan-viewer/variables.tf
@@ -22,14 +22,13 @@ variable "account_replication_type" {
   default = "ZRS"
 }
 
-variable "cognitive_deployment" {
-  type = object({
-    name          = optional(string)
+variable "cognitive_deployments" {
+  type = map(object({
     model_name    = optional(string)
     model_version = optional(string)
     model_format  = optional(string)
     sku_name      = optional(string)
     sku_capacity  = optional(number)
-  })
+  }))
   default = {}
 }
--- environments/ptl/ptl.tfvars
+++ environments/ptl/ptl.tfvars
@@ -1,5 +1,5 @@
 env = "ptl"
 
-cognitive_deployment = {
+cognitive_deployments = {
   tfplan-viewer-ptl-deployment = {}
 }
