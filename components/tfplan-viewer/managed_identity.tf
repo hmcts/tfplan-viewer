8f4e984ec26d67016bd6b35b6ecdd9c1154788ab
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-10-21 14:25:39 +0100

    replace mi

--- components/tfplan-viewer/managed_identity.tf
+++ components/tfplan-viewer/managed_identity.tf
@@ -1,5 +1,6 @@
 resource "azurerm_user_assigned_identity" "managed_identity" {
   location            = azurerm_resource_group.rg.location
-  name                = "${var.component}-${var.env}-mi"
-  resource_group_name = azurerm_resource_group.rg.name
+  name                = "${var.component}-cftptl-intsvc-mi"
+  resource_group_name = "managed-identities-cftptl-intsvc-rg"
+  tags                = module.ctags.common_tags
 }
