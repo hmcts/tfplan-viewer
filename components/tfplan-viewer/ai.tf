4b5213835e2898cf35ff323dd481b772cc07f643
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-10-21 15:03:48 +0100

    use existing storage

--- components/tfplan-viewer/ai.tf
+++ components/tfplan-viewer/ai.tf
@@ -3,6 +3,7 @@ module "ai" {
   env                           = var.env
   project                       = "${var.component}-${var.env}-project"
   existing_resource_group_name  = azurerm_resource_group.rg.name
+  existing_storage_account_name = module.storage.storageaccount_name
   common_tags                   = module.ctags.common_tags
   product                       = var.component
   component                     = var.component
