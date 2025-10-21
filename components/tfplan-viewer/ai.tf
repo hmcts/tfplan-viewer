e1d77cc99aee861db42b05fe8afd1fa30f63acea
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2025-10-21 15:10:22 +0100

    pass id instead

--- components/tfplan-viewer/ai.tf
+++ components/tfplan-viewer/ai.tf
@@ -3,7 +3,7 @@ module "ai" {
   env                           = var.env
   project                       = "${var.component}-${var.env}-project"
   existing_resource_group_name  = azurerm_resource_group.rg.name
-  existing_storage_account_name = module.storage.storageaccount_name
+  existing_storage_account_id   = module.storage.storageaccount_id
   common_tags                   = module.ctags.common_tags
   product                       = var.component
   component                     = var.component
