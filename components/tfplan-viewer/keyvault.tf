0546d936b540aa5e20aca44b3b07afc2289f74e9
Enda Kelly <80516065+endakelly@users.noreply.github.com>
2026-09-14 11:32:18 +0100

    revert branch

--- components/tfplan-viewer/keyvault.tf
+++ components/tfplan-viewer/keyvault.tf
@@ -1,5 +1,5 @@
 module "keyvault" {
-  source                  = "git::https://github.com/hmcts/cnp-module-key-vault?ref=DTSPO-34656/ado-compatibility"
+  source                  = "git::https://github.com/hmcts/cnp-module-key-vault?ref=DTSPO-31965/remove-jenkins-ptl-access"
   name                    = "${var.component}-${var.env}-kv"
   product                 = var.component
   env                     = var.env
